cee00b6e93f01885cc4be7ac2f6522a01e31e50f
AleeCF <94109004+AleeCF@users.noreply.github.com>
2023-01-27 09:02:41 +0100

    Delete CRUD directory

--- CRUD/borrar.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include("conexion.php");
-$Id = $_GET["id"];
-$base->query("DELETE FROM productos WHERE CODIGOARTICULO='$Id'");
-error_log("holaaaaaaaa" . $Id);
-header("Location:indexInicial.php");
-?>
